26c39bce39ba9e998db2b500e2fba14017e521fa
Морозов <morozov@ek.apress.ru>
2017-08-03 16:04:34 +0500

    feature(example app): fix db config

--- db/postgresql.sh
+++ db/postgresql.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+touch /data/PG_VERSION && echo 9.5 >> /data/PG_VERSION
 chmod -R 700 /data
 chown -R postgres:postgres /data
 exec /sbin/setuser postgres /usr/lib/postgresql/9.5/bin/postgres --config-file=/etc/postgresql/9.5/main/postgresql.conf
