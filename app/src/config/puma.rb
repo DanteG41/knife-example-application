7956fc7f77c71849a21d3ce8aa3fdfa71879403f
Морозов <morozov@ek.apress.ru>
2017-08-07 13:51:26 +0500

    feature(example app): add timeouts to workers

--- app/src/config/puma.rb
+++ app/src/config/puma.rb
@@ -6,6 +6,9 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
+if ENV['RACK_ENV'] == 'development'
+  worker_timeout 3600
+end
 
 on_worker_boot do
   # worker specific setup
